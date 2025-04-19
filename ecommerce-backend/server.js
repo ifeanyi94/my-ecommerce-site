7a07a975c074984de61fb7c8f6e0dc9cc9b239ab
ifeanyi94 <ifeanyienekwa@yahoo.com>
2025-04-19 14:23:01 +0100

    Update server.js with latest changes

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -92,6 +92,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { // Start the server on the specified port
     console.log(`Server is running on port ${PORT}`);
 });
