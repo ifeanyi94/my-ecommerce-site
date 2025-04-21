5601119e45714053f06474a549efd40ab8c6be57
ifeanyi94 <ifeanyienekwa@yahoo.com>
2025-04-21 11:31:45 +0100

    server.js_app.use updated

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(express.json()); // Allows us to parse JSON data
 // app.use('/images', express.static('images'));// Serve static files from the 'images' directory
 const path = require('path');
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/images', express.static(path.join(__dirname, 'photos')));
+
 
 
 const products = JSON.parse(fs.readFileSync(path.join(__dirname, 'products.json'), 'utf-8')).products;
